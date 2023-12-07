33fddb7c962731ce691cfa094b11246c96c8bbda
amith <amithkouti@gmail.com>
2023-12-08 00:03:42 +0530

    Initial Project setup , created React App using Vite and added Tailwind CSS

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='w-100 h-100 bg-slate-700'>
+      <h1 className='text-red-500'>Hello World</h1>
+    </div>
+  )
+}
+
+export default App
