ea4d55503ff307291be84ef6263ec715ef6172f4
amith <amithkouti@gmail.com>
2023-12-13 19:52:05 +0530

    Code CleanUp for PlacesPage

--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
 import { IoMdAdd } from "react-icons/io";
 import { FaCloudUploadAlt } from "react-icons/fa";
@@ -13,6 +13,34 @@ import { MdPool } from "react-icons/md";
 
 const PlacesPage = () => {
   const {action} = useParams();
+  const [title, setTitle] = useState('');
+  const [address, setAddress] = useState('');
+  const [addedPhotos, setAddedPhotos] = useState([]);
+  const [photoLink, setPhotoLink] = useState('');
+  const [description, setDescription] = useState('');
+  const [perks, setPerks] = useState([]);
+  const [extraInfo, setExtraInfoe] = useState('');
+  const [checkIn, setCheckIn] = useState('');
+  const [checkOut , setCheckOut] = useState('');
+  const [maxGuests, setMaxGuests] = useState(1);
+
+  const inputHeader = (text) => {
+    return (<h2 className='text-2xl mt-4 ml-2 font-bold'>{text}</h2>)
+  }
+
+  const inputDescription = (description) => {
+    return (<p className='text-gray-400 text-sm ml-2'>{description}</p>)
+  }
+
+  const preInput = (header, description) => {
+    return (
+      <>
+        {inputHeader(header)}
+        {inputDescription(description)}
+      </>
+    )
+  }
+
   console.log(action);
   return (
     <div>
@@ -30,14 +58,11 @@ const PlacesPage = () => {
       action === 'new' && (
         <div>
           <form>
-            <h2 className='text-2xl mt-4 ml-2 font-bold'>Title</h2>
-            <p className='text-gray-400 text-sm ml-2'>Title For Your Place, should be catchy</p>
+            {preInput("Title","Title For Your Place, should be catchy")}
             <input type='text' placeholder='title' />
-            <h2 className='text-2xl mt-4 ml-2 font-bold'>Address</h2>
-            <p className='text-gray-400 text-sm ml-2'>Address to this place</p>
+            {preInput("Address", "Address to this place")}
             <input type="text" placeholder='address' />
-            <h2 className='text-2xl mt-4 ml-2 font-bold'>Photos</h2>
-            <p className='text-gray-400 text-sm ml-2'>Pictures speak more than words</p>
+            {preInput("Photos", "Pictures speak more than words")}
             <div className='flex gap-2'>
               <input type="text" placeholder='Add Using a link.....' />
               <button className='bg-gray-300 grow px-4 rounded-2xl'>Add&nbsp;Photo</button>
@@ -48,11 +73,9 @@ const PlacesPage = () => {
                 Upload
               </button>
             </div>
-            <h2 className='text-2xl mt-4 ml-2 font-bold'>Description</h2>
-            <p className='text-gray-400 text-sm ml-2'>Describe About the place ... like Elgeant etc.</p>
+            {preInput("Description", "Describe About the place ... like Elgeant etc.")}
             <textarea  />
-            <h2 className='text-2xl mt-4 ml-2 font-bold'>Perks</h2>
-            <p className='text-gray-400 text-sm ml-2'>Select all the perks available at your place</p>
+            {preInput("Perks","Select all the perks available at your place")}
             <div className='grid gap-2 mt-2 grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
               <label className="p-4 border flex gap-2 items-center rounded-2xl cursor-pointer">
                 <input type='checkbox' />
@@ -85,12 +108,10 @@ const PlacesPage = () => {
                 <span>Pool</span>
               </label>
             </div>
-            
-            <h2 className='text-2xl mt-4 ml-2 font-bold'>Extra Info</h2>
-            <p className='text-gray-400 text-sm ml-2'>House Rules, Ambience</p>
+            {preInput("Extra Info", "House Rules, Ambience")}
             <textarea />
-            <h2 className='text-2xl mt-4 ml-2 font-bold'>Check In & Out time, max guests</h2>
-            <p className='text-gray-400 text-sm ml-2'>Add Check In And Check Out Time, remember to have some time between consecutive guests for cleaning of room.</p>
+            {preInput("Check In & Out time, max guests", "Add Check In And Check Out Time, remember to have some time between consecutive guests for cleaning of room.")}
+            
             <div className='grid gap-2 sm:grid-cols-3'>
               <div>
                 <h3 className='mt-2 ml-2 -mb-1'>Check-In</h3>
