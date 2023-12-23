d0797a1d24f2c176f02394755b65ddbd8a406d28
amith <amithkouti@gmail.com>
2023-12-23 13:47:18 +0530

    Added ShowAll Button on PLace page

--- client/src/pages/PlacePage.jsx
+++ client/src/pages/PlacePage.jsx
@@ -1,10 +1,13 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
+import { TbGridDots } from "react-icons/tb";
+
 
 const PlacePage = () => {
   const {id} = useParams();
   const [place, setPlace] = useState(null);
+  const [showAll, setShowAll] = useState(false);
   console.log(id);
   useEffect(() => {
     const fetchData = async () => {
@@ -21,25 +24,31 @@ const PlacePage = () => {
     <div className='mt-4 py-4 bg-gray-100 -mx-8 px-8'>
         <h1 className='text-2xl'>{place.title}</h1>
         <a className="block underline font-semibold" target='_blank' href={"https://maps.google.com/?q="+place.address}>{place.address}</a>
-        <div className="grid gap-2 grid-cols-[2fr_1fr] ">
-          <div>
-            {place.pics?.[0] && (
-              
-                <img className='aspect-square object-cover' src={"http://localhost:3000/uploads/"+place.pics[0]} alt=''/>
-            )}
-          </div>
-          <div className='grid '>
-            
-              {place.pics?.[1] && (
-                <img className='aspect-square object-cover' src={"http://localhost:3000/uploads/"+place.pics[1]} alt=''/>
+        <div className='relative'>
+          <div className="grid gap-2 grid-cols-[2fr_1fr] ">
+            <div>
+              {place.pics?.[0] && (
+                
+                  <img className='aspect-square object-cover' src={"http://localhost:3000/uploads/"+place.pics[0]} alt=''/>
               )}
-              <div className= 'overflow-hidden'>
-              {place.pics?.[2] && (
-                  <img className='relative top-2 aspect-square object-cover' src={"http://localhost:3000/uploads/"+place.pics[2]} alt=''/>
+            </div>
+            <div className='grid '>
+              
+                {place.pics?.[1] && (
+                  <img className='aspect-square object-cover' src={"http://localhost:3000/uploads/"+place.pics[1]} alt=''/>
                 )}
-              </div>
-            
+                <div className= 'overflow-hidden'>
+                {place.pics?.[2] && (
+                    <img className='relative top-2 aspect-square object-cover' src={"http://localhost:3000/uploads/"+place.pics[2]} alt=''/>
+                  )}
+                </div>
+              
+            </div>
           </div>
+          <button className='absolute bottom-2 right-2 p-2 bg-white border border-gray-500 hover:shadow-xl rounded-full font-serif flex items-center gap-1'>
+            <TbGridDots  />
+            Show More Photos
+          </button>
         </div>
     </div>
   )
